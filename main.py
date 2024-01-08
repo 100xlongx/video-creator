67aae6b1ffec6b0e2565859fca2d2c5d5b9bb88b
100xlong <you@example.com>
2024-01-07 19:53:36 -0500

    updated

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from moviepy.editor import ImageClip, TextClip, CompositeVideoClip, AudioClip, AudioFileClip
+from moviepy.editor import ImageClip, TextClip, CompositeVideoClip, CompositeAudioClip, AudioFileClip, concatenate_audioclips
 from dotenv import load_dotenv
 from openai_wrapper import OpenAIWrapper
 from helpers import create_image_path_from_keyword
@@ -20,15 +20,16 @@ bottom_topic = input("Enter the bottom text: ")
 
 voice_text = f"Would you rather pick {top_topic} or {bottom_topic}?"
 
-audio_filepath = openai_wrapper.generate_speech(voice_text, file_path=f"audio/{top_topic}-{bottom_topic}.mp3")
+# voiceover_filepath = openai_wrapper.generate_speech(voice_text, file_path=f"audio/{top_topic}-{bottom_topic}.mp3")
 # transcription = openai_wrapper.generate_transcription(file_path=audio_filepath, response_format="json")
-# audio_filepath = f"audio/apple-banana.mp3"
+voiceover_filepath = f"audio/rick sanchez turning himself into a pickle-travis scott fortnite skin.mp3"
 
+video_duration = 10
 top_start_time = 1
 bottom_start_time = top_start_time + 2
 
 # Load the background image as a clip
-background_clip = ImageClip('assets/tiktok_background.jpg').set_duration(10)  # duration in seconds
+background_clip = ImageClip('assets/tiktok_background.jpg').set_duration(video_duration)  # duration in seconds
 
 # Construct the paths to the downloaded images
 top_image_path = create_image_path_from_keyword(top_topic)
@@ -39,15 +40,25 @@ top_image_clip = ImageClip(top_image_path).set_duration(10).resize(height=image_
 bottom_image_clip = ImageClip(bottom_image_path).set_duration(10).resize(height=image_height).set_position(("center", y_coordinate + (image_height * 0.75))).crossfadein(1).set_start(bottom_start_time)
 
 # Create text clips for bottom, top and middle text
-txt_clip_top = TextClip(top_topic, fontsize=70, color='white', stroke_color='black', stroke_width=2, font='Verdana').set_duration(10).set_position(("center", 50)).set_start(top_start_time)
-txt_clip_bottom = TextClip(bottom_topic, fontsize=70, color='white', stroke_color='black', stroke_width=2, font='Verdana', method='caption', align='center').set_duration(10).set_position(("center", 1750)).set_start(bottom_start_time)
+txt_clip_top = TextClip(top_topic, fontsize=70, color='white', stroke_color='black', stroke_width=2, font='Verdana').set_duration(video_duration).set_position(("center", 50)).set_start(top_start_time)
+txt_clip_bottom = TextClip(bottom_topic, fontsize=70, color='white', stroke_color='black', stroke_width=2, font='Verdana', method='caption', align='center').set_duration(video_duration).set_position(("center", 1750)).set_start(bottom_start_time)
 # txt_clip_center = TextClip(transcription['text'], fontsize=50, color='white', stroke_color='black', stroke_width=2, font='Verdana').set_position(("center", "center")).set_duration(10);
 
 # add audios
-audio_clip = AudioFileClip(audio_filepath)
+voice_over = AudioFileClip(voiceover_filepath)
+gasp = AudioFileClip("audio/gasp.mp3")
+background_music = AudioFileClip('audio/byebye.mp3').volumex(0.15)
+
+while background_music.duration < video_duration:
+    background_music = concatenate_audioclips([background_music, background_music])
+
+background_music = background_music.set_duration(video_duration)
+
+final_audio = concatenate_audioclips([voice_over, gasp]).set_end(video_duration)
+composite_audio = CompositeAudioClip([final_audio, background_music])
 
 # Overlay all clips on the first clip
-final_clip = CompositeVideoClip([background_clip, top_image_clip, bottom_image_clip, txt_clip_top, txt_clip_bottom]).set_audio(audio_clip)
+final_clip = CompositeVideoClip([background_clip, top_image_clip, bottom_image_clip, txt_clip_top, txt_clip_bottom]).set_audio(composite_audio).set_duration(video_duration)
 
 # Write the result to a file
 final_clip.write_videofile(f'output/{top_topic}-{bottom_topic}.mp4', fps=24)
