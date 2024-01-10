a6827accb59dd24ed17b1c063d6102845f7ce8a4
100xlong <you@example.com>
2024-01-09 20:59:06 -0500

    refactoring

--- main.py
+++ main.py
@@ -23,13 +23,14 @@ args = parser.parse_args()
 
 # Calculate the y-coordinate for centering the image vertically
 
-top_topic = args.top_text
-bottom_topic = args.bottom_text
+top_text = args.top_text
+bottom_text = args.bottom_text
 
-top_prompt = args.top_prompt if args.top_prompt is not None else top_topic
-bottom_prompt = args.bottom_prompt if args.bottom_prompt is not None else bottom_topic
+top_prompt = args.top_prompt if args.top_prompt is not None else top_text
+bottom_prompt = args.bottom_prompt if args.bottom_prompt is not None else bottom_text
 
-output = args.output if args.output is not None else f"output/{top_topic.replace(' ', '_')}-{bottom_topic.replace(' ', '_')}.mp4"
+# default output path has underscores instead of spaces
+output = args.output if args.output is not None else f"output/{top_text.replace(' ', '_')}-{bottom_text.replace(' ', '_')}.mp4"
 
 if args.voiceover_file is not None:
     voiceover_filepath = args.voiceover_file
@@ -37,8 +38,8 @@ else:
     openai_wrapper = OpenAIWrapper(os.getenv("OPENAI_API_KEY"))
 
     voiceover_filepath = openai_wrapper.generate_speech(
-        f"Would you rather pick {top_topic} or {bottom_topic}?",
-        file_path=f"audio/{top_topic.replace(' ', '_')}-{bottom_topic.replace(' ', '_')}.mp3"
+        f"Would you rather pick {top_text} or {bottom_text}?",
+        file_path=f"audio/voice_overs/{top_text.replace(' ', '_')}-{bottom_text.replace(' ', '_')}.mp3"
     )
 
 # transcription = openai_wrapper.generate_transcription(file_path=audio_filepath, response_format="json")
@@ -60,8 +61,8 @@ top_image_clip = ImageClip(top_image_path).set_duration(10).resize(height=image_
 bottom_image_clip = ImageClip(bottom_image_path).set_duration(10).resize(height=image_height).set_position(("center", y_coordinate + (image_height * 0.75))).crossfadein(1).set_start(bottom_start_time)
 
 # Create text clips for bottom, top and middle text
-txt_clip_top = TextClip(top_prompt, fontsize=70, color='white', stroke_color='black', stroke_width=2, font='Verdana').set_duration(video_duration).set_position(("center", 50)).set_start(top_start_time)
-txt_clip_bottom = TextClip(bottom_prompt, fontsize=70, color='white', stroke_color='black', stroke_width=2, font='Verdana', method='caption').set_duration(video_duration).set_position(("center", 1750)).set_start(bottom_start_time)
+txt_clip_top = TextClip(top_text, fontsize=70, color='white', stroke_color='black', stroke_width=2, font='Verdana').set_duration(video_duration).set_position(("center", 50)).set_start(top_start_time)
+txt_clip_bottom = TextClip(bottom_text, fontsize=70, color='white', stroke_color='black', stroke_width=2, font='Verdana', method='caption').set_duration(video_duration).set_position(("center", 1750)).set_start(bottom_start_time)
 # txt_clip_center = TextClip(transcription['text'], fontsize=50, color='white', stroke_color='black', stroke_width=2, font='Verdana').set_position(("center", "center")).set_duration(10);
 
 # add audios
@@ -69,16 +70,16 @@ voice_over = AudioFileClip(voiceover_filepath)
 gasp = AudioFileClip("audio/gasp.mp3")
 background_music = AudioFileClip('audio/byebye.mp3').volumex(0.15)
 
-while background_music.duration < video_duration:
+while background_music.duration < video_duration: # repeat the background music if it's shorter than the video
     background_music = concatenate_audioclips([background_music, background_music])
 
 background_music = background_music.set_duration(video_duration)
 
-final_audio = concatenate_audioclips([voice_over, gasp]).set_end(video_duration)
-composite_audio = CompositeAudioClip([final_audio, background_music])
+combined_audio = concatenate_audioclips([voice_over, gasp]).set_end(video_duration)
+final_composite_audio = CompositeAudioClip([combined_audio, background_music])
 
 # Overlay all clips on the first clip
-final_clip = CompositeVideoClip([background_clip, top_image_clip, bottom_image_clip, txt_clip_top, txt_clip_bottom]).set_audio(composite_audio).set_duration(video_duration)
+final_clip = CompositeVideoClip([background_clip, top_image_clip, bottom_image_clip, txt_clip_top, txt_clip_bottom]).set_audio(final_composite_audio).set_duration(video_duration)
 
 # Write the result to a file
 final_clip.write_videofile(output, fps=24)
