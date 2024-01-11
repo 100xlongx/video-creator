6cb7f74814b4fae425c3269894986c092545817e
100xlong <you@example.com>
2024-01-10 19:27:20 -0500

    ok server is setup

--- tiktok_utils.py
+++ tiktok_utils.py
@@ -1,5 +1,8 @@
 import json
 import time
+import datetime
+
+from db.models import OAuthTokens
 
 def read_tokens(file_path='tiktok_tokens.json'):
     try:
@@ -20,18 +23,40 @@ def is_token_expired(file_path='tiktok_tokens.json'):
         expiry_time = token_data['obtained_at'] + token_data['expires_in']
         return current_time >= expiry_time
 
-def save_token_with_timestamp(token_response, file_path='tiktok_tokens.json'):
-    current_time = int(time.time())  # Current time in seconds since the Epoch
-    token_data = {
-        'access_token': token_response.get('access_token'),
-        'refresh_token': token_response.get('refresh_token'),
-        'expires_in': token_response.get('expires_in'),
-        'obtained_at': current_time,  # Timestamp of when the token was obtained
-        'refresh_expires_in': token_response.get('refresh_expires_in')
-    }
+def save_token_with_timestamp(token_response):
+    access_token = token_response['access_token']
+    refresh_token = token_response.get('refresh_token', None) # Not all providers return a refresh token
+    expires_in = token_response['expires_in'] # Duration in seconds
 
-    with open(file_path, 'w') as file:
-        json.dump(token_data, file, indent=4)
+    expires_at = datetime.now() + datetime.timedelta(seconds=expires_in)
+
+    # Create a new token record
+    new_token = OAuthTokens.create(
+        user_id='bot1',  # You need to determine how to set this
+        provider='tiktok',
+        access_token=access_token,
+        refresh_token=refresh_token,
+        expires_at=expires_at
+    )
+
+    # Save the record to the database
+    new_token.save()
+
+    print(new_token)
+
+
+
+    # current_time = int(time.time())  # Current time in seconds since the Epoch
+    # token_data = {
+    #     'access_token': token_response.get('access_token'),
+    #     'refresh_token': token_response.get('refresh_token'),
+    #     'expires_in': token_response.get('expires_in'),
+    #     'obtained_at': current_time,  # Timestamp of when the token was obtained
+    #     'refresh_expires_in': token_response.get('refresh_expires_in')
+    # }
+
+    # with open(file_path, 'w') as file:
+    #     json.dump(token_data, file, indent=4)
 
 # Example Usage:
 # tokens = read_tokens()
